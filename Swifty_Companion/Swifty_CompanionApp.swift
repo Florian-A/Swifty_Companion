50ca41c3349ea3dc7d8bf61b44be7e3446dbd04b
Florian <git@florian-a.dev>
2024-05-21 05:05:00 +0200

    Initial Commit

---
+++ Swifty_Companion/Swifty_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_CompanionApp.swift
+//  Swifty_Companion
+//
+//  Created by Florian ALONSO on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
